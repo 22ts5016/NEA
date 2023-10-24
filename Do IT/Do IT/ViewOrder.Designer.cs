95fed8a50071f335faf17d1f3919a34cebc44703
22ts5016 <22ts5016@barton.ac.uk>
2023-10-24 20:01:06 +0100

    Small addition to view order

--- Do IT/Do IT/ViewOrder.Designer.cs
+++ Do IT/Do IT/ViewOrder.Designer.cs
@@ -44,7 +44,14 @@
             this.OrderTypeLabel = new System.Windows.Forms.Label();
             this.OrderStatusTitleLabel = new System.Windows.Forms.Label();
             this.MainLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.OrderHeadersTable = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BarcodeHeaderLabel = new System.Windows.Forms.Label();
+            this.ImageHeaderLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.DetailsTableLayoutPanel.SuspendLayout();
+            this.OrderHeadersTable.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainMenuButton
@@ -218,16 +225,89 @@
             // MainLayoutPanel
             // 
             this.MainLayoutPanel.AutoScroll = true;
-            this.MainLayoutPanel.Location = new System.Drawing.Point(74, 235);
+            this.MainLayoutPanel.Location = new System.Drawing.Point(74, 304);
             this.MainLayoutPanel.Name = "MainLayoutPanel";
-            this.MainLayoutPanel.Size = new System.Drawing.Size(1300, 614);
+            this.MainLayoutPanel.Size = new System.Drawing.Size(1300, 545);
             this.MainLayoutPanel.TabIndex = 11;
+            // 
+            // OrderHeadersTable
+            // 
+            this.OrderHeadersTable.ColumnCount = 5;
+            this.OrderHeadersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 142F));
+            this.OrderHeadersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.OrderHeadersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 542F));
+            this.OrderHeadersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 147F));
+            this.OrderHeadersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.OrderHeadersTable.Controls.Add(this.label4, 4, 0);
+            this.OrderHeadersTable.Controls.Add(this.label5, 3, 0);
+            this.OrderHeadersTable.Controls.Add(this.BarcodeHeaderLabel, 1, 0);
+            this.OrderHeadersTable.Controls.Add(this.ImageHeaderLabel, 0, 0);
+            this.OrderHeadersTable.Controls.Add(this.label8, 2, 0);
+            this.OrderHeadersTable.Location = new System.Drawing.Point(74, 256);
+            this.OrderHeadersTable.Name = "OrderHeadersTable";
+            this.OrderHeadersTable.RowCount = 1;
+            this.OrderHeadersTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.OrderHeadersTable.Size = new System.Drawing.Size(1300, 28);
+            this.OrderHeadersTable.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1109, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(866, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Quantity";
+            // 
+            // BarcodeHeaderLabel
+            // 
+            this.BarcodeHeaderLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BarcodeHeaderLabel.AutoSize = true;
+            this.BarcodeHeaderLabel.Location = new System.Drawing.Point(184, 0);
+            this.BarcodeHeaderLabel.Name = "BarcodeHeaderLabel";
+            this.BarcodeHeaderLabel.Size = new System.Drawing.Size(47, 13);
+            this.BarcodeHeaderLabel.TabIndex = 1;
+            this.BarcodeHeaderLabel.Text = "Barcode";
+            // 
+            // ImageHeaderLabel
+            // 
+            this.ImageHeaderLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ImageHeaderLabel.AutoSize = true;
+            this.ImageHeaderLabel.Location = new System.Drawing.Point(53, 0);
+            this.ImageHeaderLabel.Name = "ImageHeaderLabel";
+            this.ImageHeaderLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ImageHeaderLabel.Size = new System.Drawing.Size(36, 13);
+            this.ImageHeaderLabel.TabIndex = 0;
+            this.ImageHeaderLabel.Text = "Image";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(507, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Product Name";
             // 
             // ViewOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.OrderHeadersTable);
             this.Controls.Add(this.MainLayoutPanel);
             this.Controls.Add(this.DetailsTableLayoutPanel);
             this.Controls.Add(this.SearchButton);
@@ -239,6 +319,8 @@
             this.Text = "ViewOrder";
             this.DetailsTableLayoutPanel.ResumeLayout(false);
             this.DetailsTableLayoutPanel.PerformLayout();
+            this.OrderHeadersTable.ResumeLayout(false);
+            this.OrderHeadersTable.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -262,5 +344,11 @@
         private System.Windows.Forms.Label OrderTypeLabel;
         private System.Windows.Forms.Label OrderStatusTitleLabel;
         private System.Windows.Forms.FlowLayoutPanel MainLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel OrderHeadersTable;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label BarcodeHeaderLabel;
+        private System.Windows.Forms.Label ImageHeaderLabel;
+        private System.Windows.Forms.Label label8;
     }
 }
